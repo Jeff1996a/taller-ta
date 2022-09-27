6f1bbb89bcbd9d11cbcd2dff33d2cdafe27e956c
jeff1996a <jefferson.alfaro@epn.edu.ec>
2022-09-27 15:34:36 -0500

    cbox

--- View/AddEquipment.php
+++ View/AddEquipment.php
@@ -95,7 +95,7 @@ die;
             <div class="mb-2 col">
                 <label for="cbEstado">Estado:</label>
                 <select class="form-select btn-outline-success" aria-label="Default select example" id="cbEstado" name="estado">
-                    <option selected>Seleccione un estado</option>
+                    <option value="" selected>Seleccione un estado</option>
                     <option value="1">Almacenado</option>
                     <option value="2">Averiado</option>
                     <option value="3">Óptimo</option>
