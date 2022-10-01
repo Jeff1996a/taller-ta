838ab5f172971ee80223e8f5ac1be75427d37e99
jeff1996a <jefferson.alfaro@epn.edu.ec>
2022-10-01 18:09:46 -0500

    text end

--- View/ListaEquipos.php
+++ View/ListaEquipos.php
@@ -12,7 +12,7 @@ die;
                 <h1><?=$GLOBALS['title']?></h1>
             </div>
             <div class="col-sm-1 mt-2 text-end">
-            <h4>Nuevo</h4>
+            <h4 clas"text-end">Nuevo</h4>
             </div>
             <div class="col-sm-1" id="btnAddEquipment" role="button" data-toggle="tooltip" data-placement="bottom" title="Nuevo equipo">
                 
