23009ead736e13da58869ea7973ee8a59d1856cb
jeff1996a <jefferson.alfaro@epn.edu.ec>
2022-10-02 16:19:45 -0500

    span

--- View/ListaEquipos.php
+++ View/ListaEquipos.php
@@ -13,9 +13,8 @@ die;
             </div>
             
             <div class="col-sm-2" id="btnAddEquipment" role="button" data-toggle="tooltip" data-placement="bottom" title="Nuevo equipo">
-                
-                <div><span><strong>Nuevo</strong></span> </div>
                 <div>
+                    <span><strong>Nuevo</strong></span>
                     <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="#ff9000" class="bi bi-plus-circle-fill float-end" viewBox="0 0 16 16">
                         <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
                     </svg>
