98151149462be58a3c074b576f4dd95f39eeb83b
jeff1996a <jefferson.alfaro@epn.edu.ec>
2022-10-02 23:14:51 -0500

    ccc

--- View/ActualizarEquipo.php
+++ View/ActualizarEquipo.php
@@ -22,7 +22,7 @@ $equipment = $GLOBALS['equipment'];
               
                 
                 <div class="col-md-3 d-flex text-right ">
-                    <button  class="btn btn-outline-success fs-5">
+                    <button type="button"  class="btn btn-outline-success fs-5">
                         <a id="btnHistorial"  class="text-success text-end" data-toggle="tooltip" data-placement="bottom" title="Historial de mantenimiento">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-calendar-week float-start  me-2" viewBox="0 0 16 16">
                                     <path d="M11 6.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm-3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm-5 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1z"/>
