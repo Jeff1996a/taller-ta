a973b2c13a0f8d94d0b5e8b630415aeba660e2d7
jeff1996a <jefferson.alfaro@epn.edu.ec>
2022-10-03 10:16:27 -0500

    button

--- View/ListaEquipos.php
+++ View/ListaEquipos.php
@@ -12,14 +12,18 @@ die;
                 <h1><?=$GLOBALS['title']?></h1>
             </div>
             
-            <div class="col-sm-2" id="btnAddEquipment" role="button" data-toggle="tooltip" data-placement="bottom" title="Nuevo equipo">
+            <div class="col-sm-1">
                 <p class="fs-5 text-warning text-right">
-                    Nuevo
-                    <a>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="#ff9000" class="bi bi-plus-circle-fill float-end ms-2" viewBox="0 0 16 16">
-                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
-                        </svg>
-                    </a>
+                    <button type="button" clas="btn btn-outline-warning" id="btnAddEquipment" data-toggle="tooltip" data-placement="bottom" title="Nuevo equipo">
+                        Nuevo
+                        <a>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
+                                <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+                                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                            </svg>
+                        </a>
+                    </button>
+                    
                 </p>
             </div>
         </div>
@@ -194,7 +198,7 @@ die;
           });
       });
 
-      //Ver el historial de un equipo
+      /*Ver el historial de un equipo
       $('#tblEquipos').on('click','#btnHistorial', function(){
           const row =  $(this).closest('tr');
           msg.id= row.find("td.idEquipo").text();
@@ -227,7 +231,7 @@ die;
                   $('#content').html(response);
               }
           });
-      });
+      });*/
 
        //Eliminar equipos
        $('#tblEquipos').on('click', '#btnEliminar', function () {
