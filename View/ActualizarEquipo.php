d58f6c5169296008c2482abd7fa940e2326b8981
jeff1996a <jefferson.alfaro@epn.edu.ec>
2022-10-03 18:23:52 -0500

    xxxx

--- View/ActualizarEquipo.php
+++ View/ActualizarEquipo.php
@@ -13,7 +13,7 @@ $equipment = $GLOBALS['equipment'];
             <div class="row">
                 <div class="col-md-9 d-flex">
                     <div id="btnRegresar" class="text-warning" role="button" data-toggle="tooltip" data-placement="bottom" title="Regresar">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-arrow-left-square-fill me-2 mt-2" viewBox="0 0 16 16">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-arrow-left-square-fill me-2 mt-3" viewBox="0 0 16 16">
                             <path d="M16 14a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12zm-4.5-6.5H5.707l2.147-2.146a.5.5 0 1 0-.708-.708l-3 3a.5.5 0 0 0 0 .708l3 3a.5.5 0 0 0 .708-.708L5.707 8.5H11.5a.5.5 0 0 0 0-1z"/>
                         </svg>
                     </div>
