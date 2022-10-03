fcf085ab4dcbf4b0326d1c151bd6b1d07ff77368
jeff1996a <jefferson.alfaro@epn.edu.ec>
2022-10-02 23:22:12 -0500

    ddddd

--- View/ActualizarEquipo.php
+++ View/ActualizarEquipo.php
@@ -21,8 +21,8 @@ $equipment = $GLOBALS['equipment'];
                 </div>
               
                 
-                <div class="col-md-3 d-flex text-right ">
-                    <button type="button"  class="btn btn-outline-success fs-6" id="btnHistorial" data-toggle="tooltip" data-placement="bottom" title="Historial de mantenimiento" >
+                <div class="col-md-3 d-flex text-right">
+                    <button type="button"  class="btn btn-outline-success btn-sm fs-6" id="btnHistorial" data-toggle="tooltip" data-placement="bottom" title="Historial de mantenimiento" >
                         <a>
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar-week float-start  me-2" viewBox="0 0 16 16">
                                 <path d="M11 6.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm-3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm-5 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1z"/>
